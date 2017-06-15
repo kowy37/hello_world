d1b20d10e82cb059c63cecb9e0eff536b5adb130
kowy37 <kullemartin@email.cz>
2017-06-15 13:57:38 +0200

    Create main.cpp
    Add factorial

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+int fact2(n,i,val) {
+  if(i==n) {
+    return val*i; 
+  }
+  return fact2(n,i++,val*i); 
+}
+int fact(int n) {
+  fact2(n,2,1);
+}
 int main() {
   std::cout << "Hello world" << std::endl
   return 0;
